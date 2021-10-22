9eb76eaf3bcbe85c14de1e95c8720678c7b1d434
Jingnong Wang <jingnongwang@Jingnongs-MBP.attlocal.net>
2021-10-21 22:50:25 -0700

    init commit

---
+++ src/test/java/com/yunpengzhihui/mybookstore/MybookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunpengzhihui.mybookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
